c93326eb4cf77ea87fd6dec2b19a21979214d108
saurshaz <saurabh@appler.xyz>
2021-02-12 05:47:24 +0000

    changes to make this working in gitpod

--- packages/demo/components/ListEntities.tsx
+++ packages/demo/components/ListEntities.tsx
@@ -1,4 +1,4 @@
-import { useNavigation } from "@react-navigation/native";
+// import { useNavigation } from "@react-navigation/native";
 import React, { useEffect, useState } from "react";
 import {
   ActivityIndicator,
@@ -14,7 +14,7 @@ import { RenderList } from "../components/RenderList";
 
 // eslint-disable-next-line @typescript-eslint/explicit-module-boundary-types
 export const ListEntities = () => {
-  const { goBack } = useNavigation();
+  // const { goBack } = useNavigation();
   const [data, setData] = useState([]);
   const [loading, setLoading] = useState(true);
 
@@ -74,7 +74,7 @@ export const ListEntities = () => {
         <Button
           accessibilityLabel="link"
           onPress={() => {
-            goBack();
+            // goBack();
           }}
           title="Go Back"
         />
