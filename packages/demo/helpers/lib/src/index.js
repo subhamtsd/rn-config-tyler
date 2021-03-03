3798be1a86a84bc176fad415365c5474031263cc
saurabh.xyz <saurabh@appler.xyz>
2021-03-03 17:17:26 +0530

    refactoring

--- packages/demo/helpers/lib/src/index.js
+++ packages/demo/helpers/lib/src/index.js
@@ -1,5 +1,5 @@
 export { JSONEditor } from "./components/JSONEditor";
 export { App } from "./container/App";
 export { rowStyle, styles } from "./styles";
-
+export * from "./helpers/utils";
 // export { render } from "./router";
