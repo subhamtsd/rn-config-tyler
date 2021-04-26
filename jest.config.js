1b0f7574c027c9ab8ea5d7ed0740323fcce42296
sagar0211 <sagarprihar002@gmail.com>
2021-04-26 13:09:28 +0530

    jest setup

--- jest.config.js
+++ jest.config.js
@@ -2,18 +2,11 @@
 const esModules = ["react-native", "@react-native-community"].join("|");
 module.exports = {
   // ...defaults,
-  preset: "./node_modules/jest-expo/jest-preset.js",
+  preset: "jest-expo",
   transformIgnorePatterns: [
     "node_modules/(?!(jest-)?react-native|react-clone-referenced-element|@react-native-community|expo(nent)?|@expo(nent)?/.*|react-navigation|@react-navigation/.*|@unimodules/.*|native-base|@storybook)",
   ],
   reporters: ["default"],
-  modulePathIgnorePatterns: [
-    // "(/__tests__/.*|(\\.|/)(preset))\\.[jt]sx?$",
-    "react-native-paper",
-    "@expo/vector-icons",
-    "react-native-vector-icons",
-  ],
-  // setupFiles: ["./tests/jestSetupFile.js"],
   transform: {
     "^.+\\.js$": "babel-jest",
   },
