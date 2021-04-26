8be5e271a6d10ae5f1ab0fd82cdf3ce013257c6e
sagar0211 <sagarprihar002@gmail.com>
2021-04-26 17:35:50 +0530

    jest config modified

--- jest.config.js
+++ jest.config.js
@@ -2,7 +2,7 @@
 const esModules = ["react-native", "@react-native-community"].join("|");
 module.exports = {
   // ...defaults,
-  preset: "./node_modules/jest-expo/jest-preset.js",
+  preset: "jest-expo",
   transformIgnorePatterns: [
     "node_modules/(?!(jest-)?react-native|react-clone-referenced-element|@react-native-community|expo(nent)?|@expo(nent)?/.*|react-navigation|@react-navigation/.*|@unimodules/.*|native-base|@storybook)",
   ],
